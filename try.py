89e99c891c65781bbeedb32bf85b5b3cf65f98ad
Idan Levi <idanlevi18@gmail.com>
2025-04-27 17:15:29 +0300

    Initial commit

---
+++ try.py
@@ -0,0 +1,8 @@
+# מבקש מהמשתמש להכניס משפט
+sentence = input("Please enter a sentence: ")
+
+# מחשב את האורך של המשפט
+length_of_sentence = len(sentence)
+
+# מציג את האורך למשתמש
+print(f"The length of your sentence is {length_of_sentence} characters.")
